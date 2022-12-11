bba2b8e7042a33e7d2889f9e4e9ec5a079367593
Daniel Ostapenko <danielostap12@gmail.com>
2022-12-10 20:49:26 -0500

    get_classes and get_by_minor

--- flask-app/src/classes/classes.py
+++ flask-app/src/classes/classes.py
@@ -33,3 +33,31 @@ def get_best_class():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
+
+@classes.route('/get_classes/<dept_name>')
+def get_all_class(dept_name):
+    cursor = db.get_db().cursor()
+    query = f'''
+        SELECT CourseNumber,Department,CourseHours
+        FROM Class
+        GROUP BY Department, CourseNumber
+        WHERE Department = '{dept_name}'
+    '''
+    cursor.execute(query)
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
--- flask-app/src/students/students.py
+++ flask-app/src/students/students.py
@@ -58,6 +58,36 @@ def get_by_major(major):
 
     return jsonify(json_data)
 
+    @students.route("/students/<minor>",methods=['GET'])
+def get_by_minor(minor):
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of students
+    cursor.execute('select FirstName, LastName, NUID, MajorName from Student where Minor = "{min}"'.format(min=minor))
+
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+#@students.route('/students/get_majors',methods=['GET'])
+#def get_majors():
+
+
 @students.route('/students/<int:advisorID>', methods=['GET'])
 def get_advisor_students(advisorID):
     cursor = db.get_db().cursor()
